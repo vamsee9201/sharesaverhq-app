1730092534aa612fc55e6987c8e349eb64ab60e4
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-16 22:52:20 -0500

    adding ride component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const testRideObject = {
+  name:'Vamsee',
+  from:'Marquis at waterview',
+  to: '1011 eagle glen dr',
+  number:'4695768978',
+  miles:14,
+  total:8
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <rideRequest/>
     </div>
   );
 }
 
+function rideRequest(rideObject) {
+  return (
+    <div>
+      <ul>
+        <li>{rideObject.name}</li>
+        <li>{rideObject.fromAddress}</li>
+        <li>{rideObject.toAddress}</li>
+        <li>{rideObject.number}</li>
+        <li>{rideObject.miles}</li>
+        <li>{rideObject.total}</li>
+      </ul>
+    </div>
+  );
+}
 export default App;
